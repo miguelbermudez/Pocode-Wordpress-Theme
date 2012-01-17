4c24ff9b3c8ccf6e4f8b788a8499cd90e4ab9043
Miguel Bermudez <miguel@miguelbermduez.com>
2012-01-17 18:20:04 -0500

    added javascript and reference files

--- source/assets/javascripts/theme.js
+++ source/assets/javascripts/theme.js
@@ -1,1 +1,30 @@
-// Regular theme JavaScript goes here
+/* Javascript for Reference Page
+ * DO an ajax load of specific divs from documenations' html
+ * ---------------------------------------------------------------------------- */
+if( $("body").hasClass("page-template-reference-php") ) {
+    var url = 'http://localhost:8888/web/pocode/docs/';
+    var filename = 'poObject.h.html';    
+    var ajaxurl = url + filename;
+    
+    //Load in google fonts
+    WebFontConfig = {
+        google: { families: [ 'Terminal+Dosis::latin' ] }
+    };
+    loadGoogleFonts();
+    
+    // //Load in index
+    // $("#index").load(ajaxurl + " #index > *");
+    // 
+    // //Load in content
+    // $("#code_content").load(ajaxurl + " #content > *");
+}
+
+function loadGoogleFonts() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+}
---
+++ source/templates/pages/reference.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for the reference page.
+ * Template Name: pcode_reference
+ * @package PotionCode 
+ */
+
+ get_header(); ?>
+
+ <div id="content" class="row">
+     <!-- <div id="index" class="clearfix">    
+     </div>
+     
+     <div id="code_content" class="row clearfix">
+     </div> -->
+     
+     <iframe src="http://www.pocode.org/docs/poObject.h.html" width="952" >  
+       <p>Your browser does not support iframes.</p>  
+     </iframe>
+</div>     
+        
+ 
+  <?php
+ get_footer(); ?>
